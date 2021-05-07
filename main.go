ac664703e14802f19ee2932c16ce98a58d13fc07
Danil Kondratiev <knightpp@protonmail.com>
2021-05-07 23:39:42 +0300

    add datetime tracking

--- main.go
+++ main.go
@@ -1,16 +1,66 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"helper/account"
+	"io"
 	"log"
 	"os"
+	"time"
 )
+
+func loadState(path string) time.Time {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Print("Error reading state file: ", err)
+		return time.Unix(0, 0)
+	}
+	defer file.Close()
+	var t time.Time
+	err = json.NewDecoder(file).Decode(&t)
+	if err != nil {
+		log.Print("Error decoding state json: ", err)
+		return time.Unix(0, 0)
+	}
+	return t
+}
+
+func saveState(path string, t time.Time) {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Print("Error creating state file: ", err)
+		return
+	}
+	defer file.Close()
+	bts, err := json.Marshal(&t)
+	if err != nil {
+		log.Print("Error encoding state json: ", err)
+		return
+	}
+	io.Copy(file, bytes.NewBuffer(bts))
+}
 
 func main() {
 	path := flag.String("file", "cookie.txt", "path to cookie file")
+	statePath := flag.String("state", "state.json", "where to save/load state data")
 	flag.Parse()
 	log.Println("Started")
+	beijing, err := time.LoadLocation("Asia/Shanghai") // "China/Beijing"
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastSignIn := loadState(*statePath)
+	nextDayAfterSignIn := lastSignIn.Add(time.Duration(24-lastSignIn.Hour()) * time.Hour)
+
+	today := time.Now().In(beijing)
+	if nextDayAfterSignIn.After(today) {
+		log.Print("The site should reset at ~", nextDayAfterSignIn,
+			" but now ", today)
+		return
+	}
+
 	cookieBytes, err := os.ReadFile(*path)
 	if err != nil {
 		log.Fatalln(err)
@@ -34,6 +84,7 @@ func main() {
 	} else {
 		log.Println("You have already signed-in today")
 	}
+	saveState(*statePath, time.Now().In(beijing))
 }
 
 // func showTotals(acc *account.Account) {
